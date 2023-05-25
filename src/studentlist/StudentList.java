e8138c4cd6f13e66621b7cd97ff7b39b861bc1c5
lenovo <lenovo@DESKTOP-5R0KNIL>
2023-05-25 18:39:16 -0400

    first commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author lenovo
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("its a student list");        
+// TODO code application logic here
+    }
+    
+}
