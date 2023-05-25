10aedebc2a4b739c3d7aa2bc1f51ab3d578acf0e
lenovo <lenovo@DESKTOP-5R0KNIL>
2023-05-25 18:47:59 -0400

    Second commit

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -13,6 +13,14 @@ public class StudentList {
     /**
      * @param args the command line arguments
      */
+    private String name;
+    
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
     public static void main(String[] args) {
       System.out.println("its a student list");        
 // TODO code application logic here
